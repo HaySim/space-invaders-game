e7a6870cb7d5c5a892e1de94a31c0bc4251aa162
HaySim <hayley_simmons@outlook.com>
2020-06-24 01:41:36 +0100

    create the player bullet

--- space_invaders.py
+++ space_invaders.py
@@ -30,6 +30,33 @@ player.setheading(90)
 
 playerspeed = 15
 
+# Create the enemy
+enemy = turtle.Turtle()
+enemy.color("red")
+enemy.shape("circle")
+enemy.penup()
+enemy.speed(0)
+enemy.setposition(-200, 250)
+
+enemyspeed = 2
+
+# Create the player's bullet
+bullet = turtle.Turtle()
+bullet.color("yellow")
+bullet.shape("triangle")
+bullet.penup()
+bullet.speed(0)
+bullet.setheading(90)
+bullet.shapesize(0.5, 0.5)
+bullet.hideturtle()
+
+bulletspeed = 20
+
+# Define bullet state
+# ready - ready to fire 
+# fire - bullet is firing
+bulletstate = "ready"
+
 # Move the player left and right
 def move_left():
     x = player.xcor()
@@ -45,11 +72,54 @@ def move_right():
         x = 280
     player.setx(x)
 
+def fire_bullet():
+    # Declare bulletstate as global
+    global bulletstate
+    if bulletstate == "ready":
+        bulletstate = "fire"
+        # Move the bullet to just above the player
+        x = player.xcor()
+        y = player.ycor() + 10
+        bullet.setposition(x, y)
+        bullet.showturtle()
+
 # Create keyboard bindings
 turtle.listen()
 turtle.onkey(move_left, "Left")
 turtle.onkey(move_right, "Right")
+turtle.onkey(fire_bullet, "space")
 
+# Main game loop
+while True:
+
+    # Move the enemy
+    x = enemy.xcor()
+    x += enemyspeed
+    enemy.setx(x)
+
+    # Move the enemy back and down
+    if enemy.xcor() > 280:
+        y = enemy.ycor()
+        y -= 40
+        enemyspeed *= -1
+        enemy.sety(y)
+
+    if enemy.xcor() < -280:
+        y = enemy.ycor()
+        y -= 40
+        enemyspeed *= -1
+        enemy.sety(y)
+
+    # Move the bullet
+    if bulletstate == "fire":
+        y = bullet.ycor()
+        y += bulletspeed
+        bullet.sety(y)
+
+    # Check to see if the bullet has gone to the top
+    if bullet.ycor() > 275:
+        bullet.hideturtle()
+        bulletstate = "ready"
 
 
 
